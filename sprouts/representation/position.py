a1cebf7c004b8fdc29c985abb81ed5c501836ebe
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-04-03 11:53:53 +0200

    Position canonalization
    The position canonalization is started with the remove dead parts method.

--- sprouts/representation/position.py
+++ sprouts/representation/position.py
@@ -1,7 +1,17 @@
 class Representation:
     @staticmethod
     def of(representation):
-        return Position()
+        return Position(representation)
 
 class Position:
-    pass
+    def __init__(self, representation):
+        self.representation = representation
+
+    def removeDeadParts(self):
+        return Position(self.representation)
+
+    def __eq__(self, other):
+        return self.representation == other.representation
+
+    def __hash__(self):
+        return hash(self.representation)
--- sprouts/representation/test/representation.py
+++ sprouts/representation/test/representation.py
@@ -6,5 +6,8 @@ class testRepresentation(unittest.TestCase):
     def testCreationOfRepresentation(self):
         self.assertNotEqual(None, Representation.of("A.B.C.}!"))
 
+    def testNumberOfRegionsOfPosition(self):
+        self.assertEqual(Representation.of("A.}!"), Representation.of("A.}!").removeDeadParts())
+
 if __name__ == '__main__':
     unittest.main()
